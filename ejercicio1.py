716a19a75a732d35353f4bff00cae89a19928ccd
andreamcm <cor16076@uvg.edu.gt>
2019-09-04 16:42:48 -0600

    Ejercicio3Real

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+#Tenemos n cantidad de generadores random y son los que sacan las funciones inversas,
+#entonces los parámetros de la función también son inversos. Dependiendo de las probabiliades
+#que saque
--- ejercicio3.py
+++ ejercicio3.py
@@ -18,10 +18,18 @@ def normal(mu, sigma):
                 return mu + sigma*y
             else:
                 return mu + sigma*y
+
+
+def uniform(mu2, sigma2):
+    rango = sigma2 - mu2
+    selection = np.random.uniform(0, 1)
+    return mu2 + (rango*selection)
     
 
 def vpn(proy, iter):
     if(proy == 1):
+        #vpn_list = []
+        vpn_net = 0
         for i in range(0, iter):
             i0 = -800
             t1 = normal(-800, 50)
@@ -30,4 +38,33 @@ def vpn(proy, iter):
             t4 = normal(300, 200)
             t5 = normal(400, 200)
             t6 = normal(500, 200)
-            uni = 
+            uni = uniform(200, 8440)
+            vpn = i0 + (t1/(1 + 0.1)**1) + (t2/(1 + 0.1)**2) + (t3/(1 + 0.1)**3) + (t4/(1 + 0.1)**4) + (t5/(1 + 0.1)**5) + (t6/(1 + 0.1)**6) + (uni/(1 + 0.1)**7)
+            vpn_net += vpn
+        return vpn_net/iter
+    if(proy == 2):
+        #vpn_list = []
+        vpn_net = 0
+        for i in range(0, iter):
+            i0 = -900
+            t1 = normal(-600, 50)
+            t2 = normal(-200, 50)
+            t3 = normal(-600, 100)
+            t4 = normal(250, 150)
+            t5 = normal(350, 150)
+            t6 = normal(400, 150)
+            uni = uniform(1600, 6000)
+            vpn = i0 + (t1/(1 + 0.1)**1) + (t2/(1 + 0.1)**2) + (t3/(1 + 0.1)**3) + (t4/(1 + 0.1)**4) + (t5/(1 + 0.1)**5) + (t6/(1 + 0.1)**6) + (uni/(1 + 0.1)**7)
+            vpn_net += vpn
+        return vpn_net/iter
+
+print("---------------- HOTEL ----------------")
+print("Con 100 iteraciones:" + "\n" + "\tVPN = " + str(vpn(1, 100)))
+print("Con 1000 iteraciones:" + "\n" + "\tVPN = " + str(vpn(1, 100)))
+print("Con 10000 iteraciones:" + "\n" + "\tVPN = " + str(vpn(1, 100)))
+
+
+print("\n------------------ CC ------------------")
+print("Con 100 iteraciones:" + "\n" + "\tVPN = " + str(vpn(2, 100)))
+print("Con 1000 iteraciones:" + "\n" + "\tVPN = " + str(vpn(2, 100)))
+print("Con 10000 iteraciones:" + "\n" + "\tVPN = " + str(vpn(2, 100)))
